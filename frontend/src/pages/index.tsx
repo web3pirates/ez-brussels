2e41e4a340a2a909d6fdafa3605157f18938b0cd
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2024-07-12 22:54:57 +0200

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -27,34 +27,38 @@ export default function Home() {
               <div className="w-full col-span-12">
                 <p className="flex justify-center w-full gap-2 pt-4 font-extrabold text-2xl md:text-3xl mb-4"></p>
                 <div className="mb-8">
-                  {opportunities.map((opportunity, index) => (
-                    <div
-                      key={index}
-                      className="flex flex-col md:flex-row justify-between items-start md:items-center w-full p-4 bg-white rounded-lg shadow-md border border-gray-200 mb-2"
-                    >
-                      <div className="flex gap-5 mb-4 md:mb-0 items-start">
-                        <Image
-                          src={`/images/${opportunity.chain}.png`}
-                          alt="Ethereum"
-                          width={60}
-                          height={60}
-                        />
-                        <p className="text-xl mt-4 font-semibold">{opportunity.name}</p>
+                  {opportunities
+                    .sort((a, b) => b.apy - a.apy)
+                    .map((opportunity, index) => (
+                      <div
+                        key={index}
+                        className={`flex flex-col md:flex-row justify-between items-start md:items-center w-full p-4 pl-6 md:pl-4 rounded-lg shadow-md border border-gray-200 mb-3 ${
+                          opportunity.apy === maxApy ? 'bg-yellow-100' : 'bg-white'
+                        }`}
+                      >
+                        <div className="flex gap-5 mb-4 md:mb-0 items-start">
+                          <Image
+                            src={`/images/${opportunity.chain}.png`}
+                            alt="Ethereum"
+                            width={60}
+                            height={60}
+                          />
+                          <p className="text-xl mt-4 font-semibold">{opportunity.name}</p>
+                        </div>
+                        <div className="flex gap-5 items-start md:items-center">
+                          <p className="text-lg font-semibold mt-2">APY: {opportunity.apy}%</p>
+                          <button
+                            onClick={() => {
+                              console.log('Supply funds');
+                            }}
+                            type="button"
+                            className={supplyFundsButtonStyle}
+                          >
+                            Supply Funds
+                          </button>
+                        </div>
                       </div>
-                      <div className="flex gap-5 items-start md:items-center">
-                        <p className="text-lg font-semibold mt-2">APY: {opportunity.apy}%</p>
-                        <button
-                          onClick={() => {
-                            console.log('Supply funds');
-                          }}
-                          type="button"
-                          className={supplyFundsButtonStyle}
-                        >
-                          Supply Funds
-                        </button>
-                      </div>
-                    </div>
-                  ))}
+                    ))}
                 </div>
               </div>
             </div>
@@ -70,16 +74,18 @@ const opportunities = [
   {
     name: 'Ethereum',
     chain: 'eth',
-    apy: '2.5',
+    apy: 2.5,
   },
   {
     name: 'Arbitrum',
     chain: 'arbitrum',
-    apy: '3.5',
+    apy: 3.5,
   },
   {
     name: 'Polygon',
     chain: 'polygon',
-    apy: '4.5',
+    apy: 4.5,
   },
 ];
+
+const maxApy = Math.max(...opportunities.map((opportunity) => opportunity.apy));
