d1d091faf593257603c417430c6f2a4987992bd3
Riksa011 <riccardobusiness04@gmail.com>
2024-07-13 12:15:35 +0200

    fetch usdc and eth balance with blockscout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,146 @@
-import AaveDataComponent from '@/components/AaveDataComponent';
-import { Footer } from '@/components/Footer';
-import { Nav } from '@/components/Nav';
-import { CustomContainer, Layout } from '@/components/atoms';
-import Head from 'next/head';
-import { useState } from 'react';
-import { FaArrowRight } from 'react-icons/fa';
-
+import AaveDataComponent from "@/components/AaveDataComponent";
+import { Footer } from "@/components/Footer";
+import { Nav } from "@/components/Nav";
+import { OpportunityComponent } from "@/components/OpportunityComponent";
+import { CustomContainer, Layout } from "@/components/atoms";
+import fetch from "cross-fetch";
+import Head from "next/head";
+import { useMemo, useState } from "react";
 // Importing an arrow icon from react-icons library
+import { FaArrowRight } from "react-icons/fa";
+import { useAsyncMemo } from "use-async-memo";
+import { useAccount } from "wagmi";
 
 export const supplyFundsButtonStyle =
-  'inline-block text-gray-900 bg-gradient-to-r from-cyan-200 to-blue-200 border border-black hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-sm min-w-20 py-2.5 font-bold text-center no-underline cursor-pointer transition-colors duration-300 ease-in-out whitespace-nowrap';
+  "inline-block text-gray-900 bg-gradient-to-r from-cyan-200 to-blue-200 border border-black hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-sm min-w-20 py-2.5 font-bold text-center no-underline cursor-pointer transition-colors duration-300 ease-in-out whitespace-nowrap";
+
+const blockscoutBaseUrl = `https://base.blockscout.com/api/v2/`;
+
+interface RawToken {
+  token: {
+    address: string;
+    decimals: number;
+    exchange_rate: number;
+    icon_url: string;
+    name: string;
+    symbol: string;
+  };
+  value: bigint;
+}
+
+interface RawEth {
+  coin_balance: bigint;
+  exchange_rate: number;
+}
+
+interface Token {
+  exchange_rate: number;
+  balance: number;
+}
+
+const USDCContractAddress =
+  "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913".toLowerCase();
 
 export default function Home() {
+  const { isConnected, address } = useAccount();
   const [showList, setShowList] = useState(false);
   const [selectedToken, setSelectedToken] = useState(-1);
+
+  const defaultEthBalance: Token = {
+    exchange_rate: 3152,
+    balance: 0,
+  };
+  const ethBalance: Token = useAsyncMemo(
+    async () => {
+      if (!isConnected || !address) return defaultEthBalance;
+      const url = `${blockscoutBaseUrl}/addresses/${address}`;
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          console.error("Blockscout eth api response was not ok");
+          return defaultEthBalance;
+        }
+
+        const data: RawEth | undefined = await response.json();
+        if (!data) {
+          console.error("failed fecthing eth balance");
+          return defaultEthBalance;
+        }
+        const balance = parseFloat(
+          (Number(data.coin_balance) / 10 ** 18).toFixed(4)
+        );
+
+        const result: Token = { exchange_rate: data.exchange_rate, balance };
+        return result;
+      } catch (error) {
+        console.error("Error fetching eth balances:", error);
+        return defaultEthBalance;
+      }
+    },
+    [address, isConnected],
+    defaultEthBalance
+  );
+
+  const defaultUsdcBalance: Token = {
+    exchange_rate: 1,
+    balance: 0,
+  };
+  const usdcBalance: Token = useAsyncMemo(
+    async () => {
+      if (!isConnected || !address) return defaultUsdcBalance;
+      const url = `${blockscoutBaseUrl}/addresses/${address}/token-balances`;
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          console.error("Blockscout tokens api response was not ok");
+          return defaultUsdcBalance;
+        }
+
+        const data: RawToken[] | undefined = await response.json();
+        if (!data) {
+          console.error("failed fecthing eth balance");
+          return defaultUsdcBalance;
+        }
+
+        const usdcToken = data.find((tokenData) => {
+          const tokenAddress = tokenData.token.address.toLowerCase();
+          return tokenAddress === USDCContractAddress;
+        });
+
+        if (!usdcToken) return defaultUsdcBalance;
+
+        const { decimals, exchange_rate } = usdcToken.token;
+        const balance = parseFloat(
+          (Number(usdcToken.value) / 10 ** decimals).toFixed(4)
+        );
+
+        return {
+          exchange_rate,
+          balance,
+        };
+      } catch (error) {
+        console.error("Error fetching eth balances:", error);
+        return defaultUsdcBalance;
+      }
+    },
+    [address, isConnected],
+    defaultUsdcBalance
+  );
+
+  const tokens = useMemo(() => {
+    return [
+      {
+        name: "ETH",
+        amount: ethBalance.balance,
+      },
+      {
+        name: "USDC",
+        amount: usdcBalance.balance,
+      },
+    ];
+  }, [ethBalance, usdcBalance]);
 
   return (
     <>
@@ -38,7 +165,9 @@ export default function Home() {
                       <div
                         key={index}
                         className={`flex flex-col md:flex-row justify-between items-start md:items-center w-full p-4 pl-6 md:pl-4 rounded-lg shadow-md border border-gray-200 mb-3
-                        ${index === selectedToken ? 'bg-yellow-100' : 'bg-white'}
+                        ${
+                          index === selectedToken ? "bg-yellow-100" : "bg-white"
+                        }
                           `}
                       >
                         <p className="text-md font-semibold">
@@ -80,18 +209,3 @@ export default function Home() {
     </>
   );
 }
-
-const tokens = [
-  {
-    name: 'ETH',
-    amount: 0.2,
-  },
-  {
-    name: 'WETH',
-    amount: 0.23,
-  },
-  {
-    name: 'USDC',
-    amount: 1000,
-  },
-];
