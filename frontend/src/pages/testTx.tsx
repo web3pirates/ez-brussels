05052d3bcabd9eb0b5a20acb4a9e12537dc8238c
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2024-07-13 16:19:08 +0200

    Add testTx page

--- frontend/src/pages/testTx.tsx
+++ frontend/src/pages/testTx.tsx
@@ -9,7 +9,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { useMemo, useState } from "react";
 // Importing an arrow icon from react-icons library
-import { FaArrowRight } from "react-icons/fa";
 import { useAsyncMemo } from "use-async-memo";
 import { useAccount } from "wagmi";
 
@@ -52,7 +51,7 @@ const USDTContractAddress =
 const WETHcontractAddress =
   "0x4200000000000000000000000000000000000006".toLowerCase();
 
-export default function Home() {
+export default function TestTx() {
   const { isConnected, address } = useAccount();
   const [showList, setShowList] = useState(false);
   const [selectedToken, setSelectedToken] = useState(-1);
@@ -266,125 +265,7 @@ export default function Home() {
         <CustomContainer as="main">
           <div className="w-full">
             <div className="flex justify-between items-center mt-5">
-              {/* <OpportunityComponent opportunity={{ chain: "8453" }} /> */}
-              <div className="flex gap-2">
-                <Button
-                  onClick={() => {
-                    window.location.href = "/positions";
-                  }}
-                >
-                  Positions
-                </Button>
-                <Button
-                  onClick={() => {
-                    window.location.href = "/transactions";
-                  }}
-                >
-                  Transactions
-                </Button>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-12 gap-4">
-              <div className="w-full col-span-6">
-                <div className="mb-8">
-                  <div className="flex flex-col gap-8 px-2 pt-5 pb-2">
-                    <p className="text-2xl">
-                      {totalUsd && (
-                        <>
-                          Balance $
-                          <span className="font-bold">
-                            {parseFloat(totalUsd).toFixed(2)}
-                          </span>
-                        </>
-                      )}
-                    </p>
-                    <p className="text-lg font-semibold">Assets to supply</p>
-                  </div>
-                  <div className="w-full">
-                    {tokens.map((token, index) => (
-                      <div
-                        key={index}
-                        className={`${tokenDivStyle} ${
-                          index === selectedToken ? "bg-yellow-100" : "bg-white"
-                        }`}
-                      >
-                        <div className="flex flex-row gap-4 items-center">
-                          <Image
-                            src={token.logoUrl}
-                            alt={`${token.name} logo`}
-                            width={35}
-                            height={35}
-                            className="rounded-full"
-                          />
-                          <p className="text-md font-semibold">
-                            {token.amount} {token.name} (${token.value})
-                          </p>
-                        </div>
-
-                        {selectedToken !== index && (
-                          <button
-                            onClick={() => {
-                              setShowList(true);
-                              setSelectedToken(index);
-                            }}
-                            type="button"
-                            className={supplyFundsButtonStyle}
-                          >
-                            <div className="flex gap-2 p-1 items-center justify-center">
-                              Select <FaArrowRight className="button-icon" />
-                            </div>
-                          </button>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-
-              {showList && (
-                <div className="w-full col-span-6 mt-[3.75rem]">
-                  <p className="flex justify-center w-full gap-2 font-extrabold text-2xl md:text-3xl mb-4"></p>
-                  <p className="text-lg p-2 font-semibold">
-                    Opportunities for {tokens[selectedToken].name}
-                  </p>
-
-                  <AaveDataComponent
-                    symbol={tokens[selectedToken].name}
-                    amount={tokens[selectedToken].amount}
-                  />
-                </div>
-              )}
-
-              {!showList && (
-                <div className="w-full col-span-6 mt-[5.3125rem]">
-                  <p className="flex justify-center w-full gap-2 pt-4 mt-1 font-extrabold text-2xl md:text-3xl mb-4"></p>
-
-                  <div
-                    className={`justify-between items-start md:items-center w-full p-4 pl-6 md:pl-4 rounded-lg shadow-md border border-gray-200 mb-3 bg-white`}
-                  >
-                    <p className="text-lg p-2 font-semibold">
-                      Select a token and start earning
-                    </p>
-                    <p className="p-2">
-                      <span className="font-semibold">EEZY</span> allows you to{" "}
-                      <span className="font-semibold">
-                        manage your assets across different chains seamlessly
-                      </span>{" "}
-                      from a single Base Smart Wallet without the hassle of
-                      switching chains or bridging funds.
-                    </p>
-                    <p className="p-2">
-                      <span className="font-semibold">EEZY</span> provides you
-                      with{" "}
-                      <span className="font-semibold">
-                        the best opportunities
-                      </span>{" "}
-                      to earn interest on your assets across different chains.
-                    </p>
-                  </div>
-                </div>
-              )}
+              <OpportunityComponent opportunity={{ chain: "8453" }} />
             </div>
           </div>
         </CustomContainer>
